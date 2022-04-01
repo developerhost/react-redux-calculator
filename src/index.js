85514ecfa9b71ab1ef630244ec087c7945c954c4
xxxx <squall0609.kanon@gmail.com>
2022-04-01 10:50:22 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+import App from './App';
+import reducer from './redux/reducers';
+
+const Store = createStore(reducer);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={Store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ src/redux/actions/types.js
@@ -0,0 +1,12 @@
+// 「数字がクリックされたとき」
+export const INPUT_NUMBER='INPUT_NUMBER';
+
+// 「+, -, 」など演算子
+export const PLUS = 'PLUS';
+export const MINUS = 'MINUS';
+export const MULTIPLY = 'MULTIPLY';
+export const DIVIDE = 'DIVIDE';
+
+export const EQUAL = 'EQUAL';
+
+export const CLEAR = 'CLEAR';
